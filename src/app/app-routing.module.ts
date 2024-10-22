1b7634b3c056f66cbd3f52527cc15a2cdcb4c44c
hedgehog13 <ira.varshavsky@gmail.com>
2024-10-22 16:23:56 -0400

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,15 @@ import { ServicesComponent } from './offered-services/services.component';
 
 const routes: Routes = [
 
-  { path: 'about', component: AboutComponent },
-  { path: 'services', component: ServicesComponent },
-  { path: 'promotions', component: PromotionsComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: '/about' } 
 ];
 const routerOptions: ExtraOptions = {
   anchorScrolling: 'enabled', // Enable anchor scrolling
-  scrollOffset: [0, 64] // Adjust this to add an offset if needed (e.g., for fixed navbar)
+  scrollOffset: [0, 64],
+
+  scrollPositionRestoration: 'enabled',
 };
 @NgModule({
   imports: [RouterModule.forRoot(routes, routerOptions)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
