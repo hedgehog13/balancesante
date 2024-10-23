2ffef6315636ee06ed9be2bd62d4139c427742cf
hedgehog13 <ira.varshavsky@gmail.com>
2024-10-23 11:27:25 -0400

    mobile fixs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,16 +22,30 @@ export class HeaderComponent {
   onWindowScroll() {
     const nav = document.querySelector('nav');
 
-  if (window.scrollY > 50) {
-    nav?.classList.add('scrolled');
-  } else {
-    nav?.classList.remove('scrolled');
-  }
+    if (window.scrollY > 50) {
+      nav?.classList.add('scrolled');
+  
+      // Check if the viewport width is less than 992px (mobile view)
+      if (window.innerWidth < 992) {
+        nav?.classList.add('mobile-scrolled'); // Add a specific class for mobile view
+      } else {
+        nav?.classList.remove('mobile-scrolled'); // Remove class if not in mobile view
+      }
+    } else {
+      nav?.classList.remove('scrolled');
+      nav?.classList.remove('mobile-scrolled'); // Ensure class is removed when scrolling back to the top
+    }
 }
   // Function to switch the language
   switchLanguage(lang: string) {
     this.currentLanguage = lang;
     this.translate.use(lang);
     localStorage.setItem('language', lang);
+  }
+  closeNavbar() {
+    const navbarCollapse = document.getElementById('navbarResponsive');
+    if (navbarCollapse) {
+      navbarCollapse.classList.remove('show');
+    }
   }
 }
